0713331057ef03dfc394b81822f82a7eef482215
RuslanZaripov <zaripovruslan864@gmail.com>
2024-03-28 18:46:28 +0300

    refactor TrOCR to process multiple image

--- src/pipeline/word_recognition_utils.py
+++ src/pipeline/word_recognition_utils.py
@@ -11,18 +11,11 @@ class TrOCR:
         self.trained_model = VisionEncoderDecoderModel.from_pretrained("raxtemur/trocr-base-ru").to(self.device)
 
     def __call__(self, images):
-        texts = []
-        for image in images:
-            # plt.imshow(image)
-            # plt.show()
+        return self.ocr(images)
 
-            text = self.ocr(image)
-            texts.append(text)
-        return texts
-
-    def ocr(self, image):
+    def ocr(self, images):
         # We can directly perform OCR on cropped images.
-        pixel_values = self.processor(image, return_tensors='pt').pixel_values.to(self.device)
+        pixel_values = self.processor(images, return_tensors='pt').pixel_values.to(self.device)
         generated_ids = self.trained_model.generate(pixel_values)
-        generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)
         return generated_text
