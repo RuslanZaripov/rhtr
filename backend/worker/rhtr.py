fe5c9d8a6cc3462ac62283918cb4c0ed05bc03ab
RuslanZaripov <zaripovruslan864@gmail.com>
2024-05-14 17:17:20 +0300

    separate broker and backend

--- backend/worker/rhtr.py
+++ backend/worker/rhtr.py
@@ -1,115 +1,14 @@
 import os
+from os import getenv
 
 import cv2
 import numpy as np
 from celery import Celery, states
-
-import src.pipeline.pipelinepredictor
-from os import getenv
-
-from redis import Redis
-from redis.exceptions import ConnectionError
-
-from kombu import Connection
-from kombu.exceptions import OperationalError
-
 from kombu import Queue
 
-
-# --------------------------- REDIS ---------------------------
-
-def get_redis_password() -> str:
-    return getenv("REDIS_PASS", "password")
-
-
-def get_redis_port() -> str:
-    return getenv("REDIS_PORT", "6379")
-
-
-def get_redis_dbnum() -> str:
-    return getenv("REDIS_DB", "0")
-
-
-def get_redis_host() -> str:
-    return getenv("REDIS_HOST", "127.0.0.1")
-
-
-def get_backend_url() -> str:
-    pw = get_redis_password()
-    port = get_redis_port()
-    db = get_redis_dbnum()
-    return "redis://{password}{hostname}{port}{db}".format(
-        hostname=get_redis_host(),
-        password=':' + pw + '@' if len(pw) != 0 else '',
-        port=':' + port if len(port) != 0 else '',
-        db='/' + db if len(db) != 0 else ''
-    )
-
-
-def is_backend_running() -> bool:
-    try:
-        conn = Redis(
-            host=get_redis_host(),
-            port=int(get_redis_port()),
-            db=int(get_redis_dbnum()),
-            password=get_redis_password()
-        )
-        conn.client_list()  # Must perform an operation to check connection.
-    except ConnectionError as e:
-        print("Failed to connect to Redis instance at %s", get_redis_host())
-        print(repr(e))
-        return False
-
-    conn.close()  # type: ignore
-
-    return True
-
-
-# --------------------------- RABBIT ---------------------------
-
-def get_rabbitmq_userpass() -> str:
-    RABBITMQ_USER = getenv("RABBITMQ_USER", "guest")
-    RABBITMQ_PASS = getenv("RABBITMQ_PASS", "guest")
-    return RABBITMQ_USER + ":" + RABBITMQ_PASS + "@" if RABBITMQ_USER else ""
-
-
-def get_rabbitmq_port() -> str:
-    RABBITMQ_PORT = getenv("RABBITMQ_PORT", "5672")
-    return ":" + RABBITMQ_PORT if RABBITMQ_PORT else ""
-
-
-def get_rabbitmq_vhost() -> str:
-    RABBITMQ_VHOST = getenv("RABBITMQ_VHOST", "")
-    return "/" + RABBITMQ_VHOST if RABBITMQ_VHOST else ""
-
-
-def get_rabbitmq_host() -> str:
-    return getenv("RABBITMQ_HOST", "127.0.0.1")
-
-
-def get_broker_url() -> str:
-    return "amqp://{userpass}{hostname}{port}{vhost}".format(
-        hostname=get_rabbitmq_host(),
-        userpass=get_rabbitmq_userpass(),
-        port=get_rabbitmq_port(),
-        vhost=get_rabbitmq_vhost()
-    )
-
-
-def is_broker_running(retries: int = 3) -> bool:
-    try:
-        conn = Connection(get_broker_url())
-        conn.ensure_connection(max_retries=retries)
-    except OperationalError as e:
-        print("Failed to connect to RabbitMQ instance at %s", get_rabbitmq_host())
-        print(str(e))
-        return False
-
-    conn.close()
-    return True
-
-
-# --------------------------- WORKER ---------------------------
+import src.pipeline.pipelinepredictor
+from .backend import is_backend_running, get_backend_url
+from .broker import is_broker_running, get_broker_url
 
 if not is_broker_running():
     exit()
